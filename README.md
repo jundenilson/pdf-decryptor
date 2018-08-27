e4e00064ba1cf861e09429ef9f799f1e63c443bb
Rafael Zuquim <rzuquim@inoa.com.br>
2018-08-27 19:35:52 -0300

    ajustes e distribuição

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pdf-decryptor
-Small exe to decrypt pdf files
+Small console application to decrypt pdf files
+
+# Usage
+
+```
+.\PdfDecryptor.exe encrypted_input.pdf decrypted_output_path.pdf pwd1 pwd2
+```
+# Requirements
+.net 4.x
+no installation required
+
+# Source code
+http://git.inoa.com.br/brokertools/pdf-decryptor
--- core/Program.cs
+++ core/Program.cs
@@ -1,60 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using iText.Kernel.Crypto;
 using iText.Kernel.Pdf;
+using static System.Console;
+using static System.String;
+using static System.Text.Encoding;
 
 namespace PdfDecryptor {
     public class Program {
+        private const string _usage = 
+@"Usage: PdfDecryptor.exe encrypted_input.pdf decrypted_output_path.pdf pwd1 pwd2...
+Returns True if the input was decrypted.";
 
         public static int Main(string[] args) {
             if (!ValidateArgs(args)) return -1;
 
+            var encryptedFile = args[0];
+            var decryptedFile = args[1];
+            var passwords = args.Skip(2).ToArray();
+
             try {
-                var encryptedFile = args[0];
-                var decryptedFile = args[1];
+                if (TryOpenWithoutPassword(encryptedFile)) { WriteLine(false); return 0; }
 
-                var passwords = args.ToList();
+                WriteLine(true);
 
-                passwords.RemoveRange(0, 2);
-
-                if (passwords.Count == 0) passwords = new List<string> { "" };
-
-                var remaingTries = passwords.Count;
-
-                while (remaingTries > 0)
-                    try { if(TryDecrypt(encryptedFile, decryptedFile, passwords[--remaingTries])) break; }
-                    catch (BadPasswordException ex) {
-                        if (remaingTries > 0) continue;
-                        Console.Error.WriteLine($"Password is required for {encryptedFile}. Provided dont match: {string.Join("-", passwords)}.");
-                        return -1;
-                    }
-                
+                foreach (var password in passwords) {
+                    try { if (TryDecrypt(encryptedFile, decryptedFile, password)) return 0; } 
+                    catch (BadPasswordException) { }
+                }
             } catch (Exception ex) {
-                Console.Error.WriteLine(ex.Message);
-                Console.Error.WriteLine(ex.StackTrace);
+                Error.WriteLine(ex.Message);
+                Error.WriteLine(ex.StackTrace);
                 return -2;
             }
-            return 0;
+
+            Error.WriteLine($"Password is required for {encryptedFile}, tried: {Join("; ", passwords)}");
+            return -1;
         }
-
-        private static bool ValidateArgs(string[] args) {
-            if (args.Length>= 2) return true;
-
-            Console.WriteLine("Usage: PdfDecryptor.exe \"encrypted_input.pdf\" \"decrypted_output.pdf\" \"password1\" \"password2\" ... ");
+        
+        private static bool ValidateArgs(IReadOnlyCollection<string> args) {
+            if (args.Count >= 2) return true;
+            WriteLine(_usage);
             return false;
         }
 
         private static bool TryDecrypt(string encryptedPath, string decryptedFilePath, string password) {
-            using (var writer = new PdfWriter(decryptedFilePath)) {
-                var reader = new PdfReader(encryptedPath,
-                    new ReaderProperties().SetPassword(Encoding.UTF8.GetBytes(password)));
-                var doc = new PdfDocument(reader, writer);
+            var readerProperties = new ReaderProperties().SetPassword(UTF8.GetBytes(password));
+            using (var writer = new PdfWriter(decryptedFilePath))
+            using (var reader = new PdfReader(encryptedPath, readerProperties))
+            using (var doc = new PdfDocument(reader, writer)) {
                 doc.Close();
                 reader.Close();
             }
             return true;
+        }
+
+        private static bool TryOpenWithoutPassword(string nonEncryptedFile) {
+            try {
+                using (var reader = new PdfReader(nonEncryptedFile))
+                using (var doc = new PdfDocument(reader)) {
+                    doc.Close();
+                    reader.Close();
+                    return true;
+                }
+            }
+            catch(Exception) { return false; }
         }
     }
 }
